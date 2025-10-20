6b2658868464bdbc27be1702370a123e89175c27
jodyboy <elite.crew66@gmail.com>
2025-10-20 09:22:35 +0700

    update Cek Usaha

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,96 @@
+'use client'
+import { motion } from "motion/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="flex flex-col w-full h-[100vh] justify-center items-center px-6 py-12 md:p-10">
+      <motion.div
+        initial={{
+          y: -100,
+          opacity: 0
+        }}
+        animate={{
+          y: 0,
+          opacity: 1
+        }}
+        transition={{
+          ease: 'circInOut',
+          duration: 1.2
+        }}
+      >
+        <Image 
+          src={"/Logo.png"}
+          width={50}
+          height={50} 
+          alt={"Cek Usaha Logo"} 
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </motion.div>
+      <div 
+        className="flex flex-col w-full h-full justify-between md:justify-center items-center gap-10"
+      >
+        <div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div 
+          className="flex flex-col justify-center items-center gap-10"
+          initial={{
+            y: 200,
+            opacity: 0
+          }}  
+          animate={{
+            y: 0,
+            opacity: 1
+          }}
+          transition={{
+            ease: 'circInOut',
+            duration: 0.8
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <h1 className="font-black md:text-7xl text-6xl text-center text-amber-400 ">
+            KENAPA HARUS
+            <br/>
+            CEK USAHA?
+          </h1>
+          <p className="text-center w-full md:w-1/2">
+            Banyak UMKM bingung harus mulai dari mana untuk berkembang. Dengan CekUsaha.id Anda bisa tahu posisi usaha saat ini dan langkah selanjutnya agar naik kelas. 🚀
+          </p>
+        </motion.div>
+        <motion.div 
+          className="flex justify-center px-6 py-2 rounded-xl font-bold w-full md:w-fit hover:cursor-pointer"
+          style={{ backgroundColor: "#FCB040" }}
+          initial={{
+            y: 100,
+            opacity: 0
+          }}  
+          animate={{
+            y: 0,
+            opacity: 1
+          }}
+          transition={{
+            ease: 'circInOut',
+            duration: 0.8
+          }}
+          whileHover={{
+            y: -10,
+            backgroundColor: "#ED9927",
+            transition: {
+              ease: 'circInOut',
+              duration: 0.3,
+            }
+          }}
+          whileTap={{
+            y: 2,
+            backgroundColor: "#FFC672",
+            transition: {
+              ease: 'circInOut',
+              duration: 0.1,
+            }
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Yuk Cek Usahamu
+        </motion.div>
+      </div>
     </div>
   );
 }
